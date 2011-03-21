b8943bc364caceadc56c8bb96b49d22f1955e9ea
zkim <zachary.kim@gmail.com>
2011-03-20 23:41:21 -0600

    Using cljs 0.2

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
-                     [cljs "0.2-SNAPSHOT"]
+                     [cljs "0.2"]
                      [lein-clojars "0.6.0"]])
