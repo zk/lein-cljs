3cfc30736d5460fef778092287b2cb35b51f7a00
zkim <zachary.kim@gmail.com>
2011-03-20 23:38:44 -0600

    Bump to 0.2

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject lein-cljs "0.2-SNAPSHOT"
+(defproject lein-cljs "0.2"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
-                     [cljs "0.2-SNAPSHOT"]])
+                     [cljs "0.2-SNAPSHOT"]
+                     [lein-clojars "0.6.0"]])
