2528ff825c80d20f215c3123cbf5cc73ad508210
zkim <zachary.kim@gmail.com>
2011-09-01 07:43:13 +0000

    Using cljs 0.2.2, bump to 0.2.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-cljs "0.2.1"
+(defproject lein-cljs "0.2.2"
   :description "Lein plugin for cljs (http://github.com/cljs)"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
-                     [cljs "0.2.1"]
+                     [cljs "0.2.2"]
                      [lein-clojars "0.6.0"]])
